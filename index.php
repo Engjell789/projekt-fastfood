9c602044624f14d6d2dd27ebb074934eee4969d6
Engjell Berisha <eb64748@ubt-uni.net>
2024-01-03 20:38:09 +0100

    login register

---
+++ index.php
@@ -0,0 +1,63 @@
+<?php 
+        include('functions.php');
+        if (!isLoggedIn()) {
+            $_SESSION['msg'] = "You must log in first";
+            header('location: login.php');
+        }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+        <title>Home</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>  
+<section class="menu">
+        <div class="nav">
+            <div class="logo"><h1><b>BLITZ</b>FOOD</h1></div>
+            <ul>
+                <li><a href="home.html">HOME</a></li>
+                <li><a href="aboutus.html">ABOUT US</a></li>
+                <li><a href="burger.html">BURGER</a></li>
+                <li><a href="pizza.html">PIZZA</a></li>
+                <li><a href="contactus.html">CONTACT US</a></li>
+            </ul>
+            
+        </div>
+    </section>
+        <div class="header">
+                <h2>Home Page</h2>
+        </div>
+        <div class="content">
+                <!-- notification message -->
+                <?php if (isset($_SESSION['success'])) : ?>
+                        <div class="error success" >
+                                <h3>
+                                        <?php 
+                                                echo $_SESSION['success']; 
+                                                unset($_SESSION['success']);
+                                        ?>
+                                </h3>
+                        </div>
+                <?php endif ?>
+                <!-- logged in user information -->
+                <div class="profile_info">
+                        <img src="images/user_profile.png"  >
+
+                        <div>
+                                <?php  if (isset($_SESSION['user'])) : ?>
+                                        <strong><?php echo $_SESSION['user']['username']; ?></strong>
+
+                                        <small>
+                                                <i  style="color: #888;">(<?php echo ucfirst($_SESSION['user']['user_type']); ?>)</i> 
+                                                <br>
+                                                <a href="index.php?logout='1'" style="color: red;">logout</a>
+                                        </small>
+
+                                <?php endif ?>
+                        </div>
+                </div>
+        </div>
+</body>
+</html>
+                                
